c72274540045a1ba9dc7b989e501bae18bf6bd1b
student <student@gnu.ac.in>
2023-07-14 12:33:08 +0530

    Practical-1

---
+++ src/main/kotlin/Practical-1.1.kt
@@ -0,0 +1,22 @@
+fun main(){
+    var a=5
+    println("$a: is integer value")
+    var b=1.3
+    println("$b: is float value")
+    var c=23.24
+    println("$c: is double value")
+    var d=12345
+    println("$d: is long value")
+    var e=127
+    println("$e: is byte value")
+    var f=-3
+    println("$f: is short value")
+    val g: Char='a'
+    println("$g: is character value")
+    val s: String="xyz"
+    println("$s: is string value")
+    val h:Boolean= true
+    println("$h: is boolean value")
+
+
+}
