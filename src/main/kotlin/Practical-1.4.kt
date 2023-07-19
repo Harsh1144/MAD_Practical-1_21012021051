3744bddc2adc37f047b62203b6bfe5869a65fa43
student <student@gnu.ac.in>
2023-07-19 09:00:29 +0530

    Practical

---
+++ src/main/kotlin/Practical-1.4.kt
@@ -0,0 +1,13 @@
+import java.util.function.ToIntBiFunction
+
+fun main(){
+    println("Enter number=")
+   val a = readLine()!!.toInt()
+    if (a % 2==0){
+        println("$a: is even number")
+    }
+    else{
+        println("$a:is odd number")
+    }
+
+}
---
+++ src/main/kotlin/Practical-1.5.kt
@@ -0,0 +1,23 @@
+fun main(){
+    println("enter month number")
+    val x= readLine()!!.toString()
+   when(x){
+       1.toString() -> println("Jan")
+       2.toString() -> println("Feb")
+       3.toString() -> println("Mar")
+       4.toString() -> println("Apr")
+       5.toString() -> println("May")
+       6.toString() -> println("Jun")
+       7.toString() -> println("Jul")
+       8.toString() -> println("Aug")
+       9.toString() -> println("Sep")
+       10.toString() -> println("Oct")
+       11.toString() -> println("Nov")
+       12.toString() -> println("Dec")
+       else
+
+
+
+}
+
+}
