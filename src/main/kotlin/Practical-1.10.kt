33f7332e5fb62fc9822be350dad51830c8c6b889
strudent <student@gnu.ac.in>
2023-08-07 21:17:07 +0530

    Practical

--- src/main/kotlin/Practical-1.10.kt
+++ src/main/kotlin/Practical-1.10.kt
@@ -1,79 +1,56 @@
-class Car1 (var type: String, var name: String, var model: String, var price: String, var owner: String, var milesDrive: String) {
-    fun displayCarInfo() {
-        println("\n------Car-1 Information------")
-        println("Type: $type")
-        println("Name: $name")
-        println("Model: $model")
-        println("price: $price")
-        println("Owner name: $owner")
-        println("Miles drove: $milesDrive")
+package `practical-1`
+class Car
+(
+        var type : String,
+        var model : Int,
+        var Oprice : Double,
+        var Cprice : Double,
+        var owner : String,
+        var milesdrive : Int
+)
+{
+    init
+    {
+        println("Object of class is created and Init is Called.")
+        println()
     }
-}class Car2{
-    var type = ""
-    var name = ""
-    var model = ""
-    var price = ""
-    var owner = ""
-    var milesDrive = ""
-    init{
-        println("\nEnter the details of car-2")
-        print("Enter vehicle type: ")
-        type = readln()
-        print("Enter name: ")
-        name = readln()
-        print("Enter model: ")
-        model = readln()
-        print("Enter owner name: ")
-        owner = readln()
-        print("Enter price: ")
-        price = readln()
-        print("Enter mileDrove: ")
-        milesDrive = readln()
+    fun getOriginalCarPrice(): Double
+    {
+        return Oprice
     }
-    fun displayCarInfo(){
-        println("\n------Car-2 Information------")
-        println("Type: $type")
-        println("Name: $name")
-        println("Model: $model")
-        println("Owner name: $owner")
-        println("Miles drove: $milesDrive")
+
+    fun getCurrentCarPrice(): Double
+    {
+        return Cprice
+    }
+
+    fun displayCarInfo()
+    {
+        val originalPrice = getOriginalCarPrice()
+        val currentPrice = getCurrentCarPrice()
+        println("-----------------------------------")
+        println("Car Information : $type, $model")
+        println("Car Owner : $owner")
+        println("Miles Drive : $milesdrive")
+        println("Original Car Price : $originalPrice")
+        println("Current Car Price : $currentPrice")
+        println("-----------------------------------")
     }
 }
-class Car3{
-    var type = ""
-    var name = ""
-    var model = ""
-    var price = ""
-    var owner = ""
-    var milesDrive = ""
-    constructor (type: String,name: String,model: String,price: String,owner: String,milesDrive: String){
-        this.type = type
-        this.name = name
-        this.model = model
-        this.price = price
-        this.owner = owner
-        this.milesDrive = milesDrive
-    }
-    fun getCarPrice(){
-        println("Price: $price")
-    }
-    fun displayCarInfo(){
-        println("\n------Car-3 Information------")
-        println("Type: $type")
-        println("Name: $name")
-        println("Model: $model")
-        println("price:$price")
-        println("Owner name: $owner")
-        println("Miles drove: $milesDrive")
-    }
-}
-fun main(args: Array<String>) {
-    var c1 = Car1("4-Wheeler","Kia","Hybrid","1200000","harsh","12000")
-    c1.displayCarInfo()
 
-    var c2 = Car2()
-    c2.displayCarInfo()
+fun main()
+{
+    var car1 = Car("BMW",2018, 100000.0,98950.0, "Aman",105 )
+    var car2 = Car("BMW",2019, 400000.0,399800.0, "Karan",20 )
+    car1.displayCarInfo()
+    car2.displayCarInfo()
 
-    var c3 = Car3("4-Wheeler","Alcazar","XUV","2300000","xyz","12000")
-    c3.displayCarInfo()
+    var cars = ArrayList<Car>()
+    cars.add(Car("Toyota",2017, 1080000.0,1079000.0, "KJS",100 ))
+    cars.add(Car("Maruti",2020, 4000000.0,3998000.0, "NPP",200 ))
+    println("********** ArrayList of Car **********")
+    for (car in cars) {
+        car.displayCarInfo()
+        println()
+    }
 }
